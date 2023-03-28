2c497446f73b90d27e85e7161817af4bc8a649fe
shirleen <shirleenoey@gmail.com>
2023-03-28 16:33:42 +0700

    update ssl

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: {rejectUnauthorized: false},
+    ssl: true,
     host : process.env.DATABASE_HOST,
     port : 5432,
     user : process.env.DATABASE_USER,
