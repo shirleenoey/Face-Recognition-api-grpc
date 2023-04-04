a7064f470cb79f994657a03b2e7f1c933f07070f
shirleen <shirleenoey@gmail.com>
2023-04-04 13:22:49 +0700

    changing database

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: true,
+    ssl: { rejectUnauthorized : false },
     host : process.env.DATABASE_HOST,
     port : 5432,
     user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PWD,
+    password : process.env.DATABASE_PW,
     database : process.env.DATABASE_DB
   }
 });
